f02879aca78ac310602a795bd9f68e0b1195eca1
ericamendez <ecmendez25@gmail.com>
2023-10-26 11:06:16 -0400

    getting arrows to work, they are now contained in the leftContainer

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log(document.querySelector('code'))
+
+document.querySelector('code').addEventListener("mouseover", (event) => {
+    console.log('hi')
+    // document.querySelector('#line1').style = 'background:rgba(255,255,0,0.3)'
+})
